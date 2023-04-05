7f05d1d75cd024dc272c4fa8b88a31de1535c5c9
Kyuhee Lee <kyuhee.lee.1011@gmail.com>
2023-04-05 01:51:32 -0400

    Dom Lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+
+const mainRemove=document.querySelector('main#main')
+mainRemove.remove ();
+
+const newHeader = document.createElement('h1')
+newHeader.setAttribute ('id', 'victory');
+newHeader.innerHTML= "Kyuhee is the champion"
